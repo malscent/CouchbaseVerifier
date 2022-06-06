cdbad416b578e7d7064847a957c680616e14fba9
Justin Ashworth <justin.ashworth@couchbase.com>
2022-06-06 10:00:01 -0400

    Improvements to CI/CD

--- publish.sh
+++ publish.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
-
-export TEST_PROJECT="./CouchbaseVerifier.Tests/CouchbaseVerifier.Tests.csproj"
-export NUGET_SERVER="https://api.nuget.org/v3/index.json"
-export BUILD_DIR="./build"
-export TOOL_DIR="./CouchbaseVerifier/"
-export TOOL_PROJECT_NAME="CouchbaseVerifier.csproj"
-
 set -eu
 
+SCRIPT_SOURCE=${BASH_SOURCE[0]/%publish.sh/}
+
+xmlStarlet=$(which xml)
+if [[ -z "$xmlStarlet" ]]; then
+    echo "XmlStarlet is required for this script.  Please install it before proceeding."
+    exit 1
+fi
+
+export TEST_PROJECT="${SCRIPT_SOURCE}CouchbaseVerifier.Tests/CouchbaseVerifier.Tests.csproj"
+export NUGET_SERVER="https://api.nuget.org/v3/index.json"
+export BUILD_DIR="${SCRIPT_SOURCE}build"
+export TOOL_DIR="${SCRIPT_SOURCE}CouchbaseVerifier/"
+export TOOL_PROJECT_NAME="CouchbaseVerifier.csproj"
+
+
 project=$(basename -s .csproj "$TOOL_PROJECT_NAME")
-version=$(sed -n 's:.*<Version>\(.*\>)</Version.*:\1:p' "$TOOL_DIR$TOOL_PROJECT_NAME")
-tool_file="./build/$project.$version.nukpg"
+version=$(sed -n 's:.*<Version>\(.*\)</Version>.*:\1:p' "$TOOL_DIR$TOOL_PROJECT_NAME")
+tool_file="${SCRIPT_SOURCE}build/$project.$version.nukpg"
 
 echo "Project: $project"
 echo "Testing $project version: $version"
